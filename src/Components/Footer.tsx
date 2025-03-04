767308aa4886b6598ae0ecea8197a3e543bf66d3
AymanSdq <sedqiayman02@gmail.com>
2025-03-04 17:05:54 +0000

    Adding footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,47 +1,50 @@
-import React from "react";
+
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-      <aside>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
-        <p>
-          ACME Industries Ltd.
-          <br />
-          Providing reliable tech since 1992
-        </p>
-      </aside>
+    <>
+      <footer className="footer md:px-16  sm:footer-horizontal py-15 text-white bg-[#7C6A46] ">
+        <aside className="flex flex-col gap-4">
+          <h1 className="font-bold dancing-script text-3xl -font-script ">Paradise view</h1>
+          <p className="w-[277px] text-xs raleway text-justify">
+            The service at the Hotel Monteleone was exceptional. There was absolutely no issue that was not addressed timely and with satisfactory results. We were particulary impressed with how the hotel staff anticipated our needs (periodically coming by the Board Room to check with us)
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title raleway">Quick links</h6>
+          <a className="link link-hover raleway">Room booking</a>
+          <a className="link link-hover raleway">Rooms</a>
+          <a className="link link-hover raleway">Contact</a>
+          <a className="link link-hover raleway">Explore</a>
+        </nav>
       <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <h6 className="footer-title raleway">Company</h6>
+        <a className="link link-hover raleway">Privacy policy</a>
+        <a className="link link-hover raleway">Refund policy</a>
+        <a className="link link-hover raleway">F.A.Q</a>
+        <a className="link link-hover raleway">About</a>
       </nav>
       <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <h6 className="footer-title raleway">Social media</h6>
+        <a className="link link-hover raleway">Facebook</a>
+        <a className="link link-hover">Twitter</a>
+        <a className="link link-hover raleway">Instagram</a>
+        <a className="link link-hover raleway">Linkedin</a>
       </nav>
       <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <h6 className="footer-title raleway">Newsletter</h6>
+        <p className="text-xs w-[234px] raleway">Kindly subscribe to our newsletter to get latest deals on our rooms and vacation discount.</p>
+        <div className="my-4">
+          <input type="" placeholder="Enter your email" className="bg-white text-black rounded-md" />
+        </div>
       </nav>
     </footer>
+    <footer className="footer border-t border-[#D9D9D9] sm:footer-horizontal raleway footer-center text-white bg-[#7C6A46] py-12">
+      <aside>
+        <p className="raleway font-semibold">Paradise view 2023</p>
+      </aside>
+    </footer>
+    </>
   );
 };
 
